0b71e19a06c712f098271c84d3a6f79f36bfdd4a
Timothy Wikedzi <timothy.wikedzi@andela.com>
2020-10-11 20:16:33 +0300

    add comments to Class methods

--- Money.php
+++ Money.php
@@ -8,15 +8,30 @@ class Money {
         "USD" => 2,
     ];
 
+    /**
+     * Money constructor.
+     * @param $currency
+     * @param $value
+     */
     public function __construct($currency, $value) {
         $this->currency = $currency;
         $this->setValue($value);
     }
 
+    /**
+     * Call this function to check if the currency values matches the calling currency
+     * @param $currency
+     * @return bool
+     */
     public function currencyIs($currency) {
         return $this->currency === strtoupper($currency);
     }
 
+    /**
+     * This represents the number of decimal point for a given currency. Currentcy points could be stored and accessed
+     * as setting values when dealing with money values
+     * @return int|mixed
+     */
     public function getPoints() {
        $currencies = array_keys($this->cent_points);
        if (in_array($this->currency, $currencies)) {
@@ -25,20 +40,36 @@ class Money {
        return 0;
     }
 
+    /**
+     * Call this function to immediately convert the money value to an integer value
+     * @param $value
+     */
     public function setValue($value) {
         $this->value  = intval($value * pow(10, $this->getPoints()));
     }
 
+    /**
+     * Call this function to preprocess and extract the original value
+     * @return string
+     */
     public function getRealValue() {
         $value = $this->value / pow(10, $this->getPoints());
         return sprintf("%.{$this->getPoints()}f", $value);
     }
 
+    /**
+     * @param int $val
+     * @return string
+     */
     public function multiply($val=0) {
         $this->value *= $val;
         return $this->getRealValue();
     }
 
+    /**
+     * @param int $val
+     * @return null|string
+     */
     public function division($val=1) {
         if($val == 0){
             //Throw an error
@@ -48,6 +79,10 @@ class Money {
         return  $this->getRealValue();
     }
 
+    /**
+     * @param Money $money
+     * @return null|string
+     */
     public function sum(Money $money) {
         if(!$money->currencyIs($this->currency)) {
             //Throw an error
@@ -57,6 +92,10 @@ class Money {
         return  $this->getRealValue();
     }
 
+    /**
+     * @param Money $money
+     * @return string
+     */
     public function subtract(Money $money) {
         if(!$money->currencyIs($this->currency)) {
             //Throw an error
@@ -65,6 +104,9 @@ class Money {
         return  $this->getRealValue();
     }
 
+    /**
+     * @return string
+     */
     public function __toString()
     {
         return sprintf("%s %s\n", $this->currency, $this->getRealValue());
