2485d666ab86458e2674016e9a612d3d3d1575ef
Timothy Wikedzi <timothy.wikedzi@andela.com>
2020-10-11 16:36:44 +0300

    first commit

---
+++ currencies.php
@@ -0,0 +1,22 @@
+<?php
+class CurrencyManager {
+
+    var $price;
+    var $title;
+
+    public function multiply($par){
+
+    }
+
+    public function division(){
+
+    }
+
+    public function sum($par){
+
+    }
+
+    public function subtrack(){
+
+    }
+}
